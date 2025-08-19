54318b32d322d24bb8178124bc310bf3bcc9b53e
MSI <tulqinurinov005@gmail.com>
2025-08-19 17:27:10 +0500

    defregrf

--- data/specialization/models.py
+++ data/specialization/models.py
@@ -8,9 +8,21 @@ if TYPE_CHECKING:
 
 
 class Specialization(BaseModel):
-    code = models.CharField(max_length=50, verbose_name="Mutaxassislik kodi")
-    name = models.CharField(max_length=255, verbose_name="Mutaxassislik nomi")
-    faculty: "Faculty" = models.ForeignKey("faculty.Faculty", on_delete=models.CASCADE, related_name="specializations")
+    code = models.CharField(
+        max_length=50,
+        verbose_name="Mutaxassislik kodi"
+    )
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Mutaxassislik nomi"
+    )
+
+    faculty: "Faculty" = models.ForeignKey(
+        "faculty.Faculty",
+        on_delete=models.CASCADE,
+        related_name="specializations"
+    )
 
     def __str__(self):
         return f"{self.name} ({self.code})"
--- data/student/models.py
+++ data/student/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from typing import TYPE_CHECKING
 
-# Create your models here.
+from data.common.models import BaseModel
+
+if TYPE_CHECKING:
+    from data.specialization.models import Specialization
+
+
+class Student(BaseModel):
+    full_name = models.CharField(
+        max_length=255,
+        verbose_name="Talaba F.I.Sh"
+    )
+
+    jshshir = models.CharField(
+        max_length=50,
+        verbose_name="JSHSHIR",
+        unique=True
+    )
+
+    status = models.CharField(
+        max_length=50,
+        verbose_name="Talaba statusi",
+        default="O‘qimoqda"
+    )
+
+    specialization: "Specialization" = models.ForeignKey(
+        "specialization.Specialization",
+        on_delete=models.CASCADE,
+        related_name="students"
+    )
+
+    course = models.CharField(
+        max_length=50,
+        verbose_name="Talaba kursi"
+    )
+
+    education_type = models.CharField(
+        max_length=50,
+        verbose_name="Taʼlim turi"
+    )  # Bakalavr, Magistr
+
+    education_form = models.CharField(
+        max_length=50,
+        verbose_name="Taʼlim shakli"
+    )  # Sirtqi, Kunduzgi
+
+    group = models.CharField(
+        max_length=50,
+        verbose_name="Gruhi"
+    )
+
+    def __str__(self):
+        return self.full_name
