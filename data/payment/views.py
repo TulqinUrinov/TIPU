96af93bcc8d79af726b88a84bd31d73c55b3f562
MSI <tulqinurinov005@gmail.com>
2025-08-26 12:11:37 +0500

    dwefretgrbdfd

--- data/payment/views.py
+++ data/payment/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import PermissionDenied
 
 from data.common.permission import IsAuthenticatedUserType
 
 from rest_framework import viewsets, mixins
 from .models import InstallmentPayment
 from .serializers import InstallmentPaymentSerializer
+from ..student.models import Student
 
 
 # class InstallmentPaymentViewSet(mixins.CreateModelMixin,
@@ -18,10 +20,14 @@ class InstallmentPaymentViewSet(viewsets.ModelViewSet):
     serializer_class = InstallmentPaymentSerializer
     permission_classes = [IsAuthenticatedUserType]
 
+
     def get_queryset(self):
         # STUDENT faqat o'zini ko'rsin
         if getattr(self.request, "role", None) == "STUDENT" and self.request.student_user:
-            return InstallmentPayment.objects.filter(student=self.request.student_user)
+            student = Student.objects.filter(user_account=self.request.student_user).first()
+            if not student:
+                raise PermissionDenied({"detail": "User not found", "code": "user_not_found"})
+            return InstallmentPayment.objects.filter(student=student)
 
         # ADMIN barchasini ko'rishi yoki student_id bo'yicha filter
         queryset = InstallmentPayment.objects.all()
@@ -29,3 +35,15 @@ class InstallmentPaymentViewSet(viewsets.ModelViewSet):
         if student_id:
             queryset = queryset.filter(student_id=student_id)
         return queryset
+
+    # def get_queryset(self):
+    #     # STUDENT faqat o'zini ko'rsin
+    #     if getattr(self.request, "role", None) == "STUDENT" and self.request.student_user:
+    #         return InstallmentPayment.objects.filter(student=self.request.student_user)
+    #
+    #     # ADMIN barchasini ko'rishi yoki student_id bo'yicha filter
+    #     queryset = InstallmentPayment.objects.all()
+    #     student_id = self.request.GET.get("student")
+    #     if student_id:
+    #         queryset = queryset.filter(student_id=student_id)
+    #     return queryset
